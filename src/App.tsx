9c76c9fca9e9fe6e526a1060195b5dddd58ce3fd
Sean O’Sullivan <sosullivan425@gmail.com>
2025-05-10 20:12:09 -0400

    first attempt at matrix entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MatrixHolder } from './components/MatrixHolder';
 
 function App() {
   const [size, setsize] = useState<number>(5);
+  const [entries, setentries] = useState<[[number]]>([[0]]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MatrixHolder size={size} setsize={setsize} matrixEntries={entries} setEntries={setentries}></MatrixHolder>
     </div>
   );
 }
